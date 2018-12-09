ab81605638de62d706610210d1dd0e6a10fcedf2
acuriousowl <33234127+acuriousowl@users.noreply.github.com>
2018-12-09 18:10:13 -0500

    Changes

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 (function() {
 
-	var app = angular.module("mainApp", ["ngRoute"])
+  var app = angular.module("mainApp", ["ngRoute"])
 
-	app.config(function($routeProvider){
-		$routeProvider
-			.when("/main", {
-				templateUrl: "main.html",
-				controller: "MainPageControls"
-			})
-			.otherwise({redirectTo: "/home"})
-	})
+  app.config(function($routeProvider){
+    $routeProvider
+      .when("/main", {
+        templateUrl: "main.html",
+        controller: "MainPageControls"
+      })
+      .otherwise({redirectTo: "/main"})
+  })
 
 }())
